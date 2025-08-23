9f300a5f0d5894e095a484158175912580f13eb8
Formula <ali88elsa96@gmail.com>
2025-08-23 21:10:15 +0330

    The intro is now fully dynamic with slide images fetched online from a JSON file.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,7 @@
-
 // Slowing down link execution to fix animation issues on touch screens.
 
-document.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', function (e) {
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
 
     const href = this.href;
@@ -13,145 +12,155 @@ document.querySelectorAll('a').forEach(link => {
   });
 });
 
-
-
-
 // burger menu
 
-let burger = document.getElementById("burger")
-let burgerBox = document.getElementById("burger-box")
-let openBurgerBox = false
+let burger = document.getElementById("burger");
+let burgerBox = document.getElementById("burger-box");
+let openBurgerBox = false;
 
 // open burger menu
 burger.addEventListener("click", () => {
   if (openBurgerBox) {
-    openBurgerBox = false
-    burgerBox.style = ""
+    openBurgerBox = false;
+    burgerBox.style = "";
+  } else {
+    openBurgerBox = true;
+    burgerBox.style.right = "0%";
   }
-  else {
-    openBurgerBox = true
-    burgerBox.style.right = "0%"
-  }
-})
-
+});
 
 // Dropdown in burger menu
-let burgerDropdown = document.getElementById("burger-dropdown")
-let burgerDropdownBox = document.getElementById("burger-dropdown-box")
+let burgerDropdown = document.getElementById("burger-dropdown");
+let burgerDropdownBox = document.getElementById("burger-dropdown-box");
 
-let openBurgerDropdown = false
+let openBurgerDropdown = false;
 
 // open Dropdown in burger menu
 burgerDropdown.addEventListener("click", () => {
   if (openBurgerDropdown) {
-    openBurgerDropdown = false
-    burgerDropdownBox.style.height = ""
+    openBurgerDropdown = false;
+    burgerDropdownBox.style.height = "";
+  } else {
+    openBurgerDropdown = true;
+    burgerDropdownBox.style.height = burgerDropdownBox.scrollHeight + "px";
   }
-  else {
-    openBurgerDropdown = true
-    burgerDropdownBox.style.height = burgerDropdownBox.scrollHeight + "px"
-  }
-})
+});
+
+
 
 
 // intro slider
 
-let rightBtnIntro = document.getElementById("rightBtnIntro")
-let leftBtnIntro = document.getElementById("leftBtnIntro")
+// this for data 
+let dataIntro
+let sizeDataIntro;
+
+// this for small circles at the bottom of the slides
+let btnIntoBox = document.getElementById("btns-intro");
+let btnIntro;
+
+// In order: for accessing the element that contains the slides, the slides themselves, and the slide position.
+let slidersBox = document.getElementById("boxSlider");
+let sliders;
+let positionSlides = 0;
+
+// for right and left btn
+let rightBtnIntro;
+let leftBtnIntro;
 
 
-let btnIntro = document.getElementsByClassName("btn-intro")
-
-let sliders = Array.from(document.getElementsByClassName("sliders"))
-let positionSlides = 0
-
-
-
-// Slider change button.
-
+// For changing the style of the slider’s circular buttons and switching slides.
 function introBtnClick(num) {
-  for (let i = 0; i < 3; i++) {
-    btnIntro[i].classList.remove("active-btn")
+  for (let i = 0; i <= sizeDataIntro; i++) {
+    btnIntro[i].classList.remove("active-btn");
   }
-  btnIntro[num].classList.add("active-btn")
+  btnIntro[num].classList.add("active-btn");
 
-  if (num == 0) {
-    sliders.forEach((value) => {
-      value.style.left = "0%"
-    })
-    positionSlides = 0
-  }
-  else if (num == 1) {
-    sliders.forEach((value) => {
-      value.style.left = "100%"
-    })
-    positionSlides = 1
-  }
-  else {
-    sliders.forEach((value) => {
-      value.style.left = "200%"
-    })
-    positionSlides = 2
-  }
+  sliders.forEach((value) => {
+    value.style.left = `${num * 100}%`;
+  });
+  positionSlides = num;
 }
 
 
-// Slider R or L button.
+// For changing slides with the left and right buttons.
 function RL_slider(direction) {
-  console.log("HI");
-  
   if (direction == "R") {
-    if(positionSlides == 0){
-      introBtnClick(2)
-    }
-    else if(positionSlides == 1){
-      introBtnClick(0)
-    }
-    else{
-      introBtnClick(1)
+    if (positionSlides == 0) {
+      introBtnClick(sizeDataIntro);
+    } else {
+      introBtnClick(positionSlides - 1);
     }
   }
   if (direction == "L") {
-    if(positionSlides == 0){
-      introBtnClick(1)
-    }
-    else if(positionSlides == 1){
-      introBtnClick(2)
-    }
-    else{
-      introBtnClick(0)
+    if (positionSlides == sizeDataIntro) {
+      introBtnClick(0);
+    } else {
+      introBtnClick(positionSlides + 1);
     }
   }
 }
 
-rightBtnIntro.addEventListener("click",() => {
-  RL_slider("R")
-})
-leftBtnIntro.addEventListener("click",() => {
-  RL_slider("L")
-})
+// Getting intro information.
+fetch("https://jsonkeeper.com/b/JOWOV")
+  .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+
+    dataIntro = data;
+    sizeDataIntro = dataIntro.length - 1;
+
+    // For placing the slider image.
+    dataIntro.map((value) => {
+      slidersBox.innerHTML += `<a href="${value.href}"><img src="${value.img}" alt="${value.alt}" class="sliders img-fluid"></a>`;
+    });
+
+    // For get sliders
+    // We converted it to an array so we could use forEach on it.
+    sliders = Array.from(document.getElementsByClassName("sliders"));
+
+    // For creating circular buttons.
+    for (let i = 0; i <= sizeDataIntro; i++) {
+      if (i == 0) {
+        btnIntoBox.innerHTML = `<div class="btn-intro active-btn" onclick="introBtnClick(0)"></div>`;
+      } else {
+        btnIntoBox.innerHTML += `<div class="btn-intro" onclick="introBtnClick(${i})"></div>`;
+      }
+    }
+
+    // For get circular buttons
+    btnIntro = document.getElementsByClassName("btn-intro");
+
+    
+
+    // For get for get right and left btn
+    rightBtnIntro = document.getElementById("rightBtnIntro");
+    leftBtnIntro = document.getElementById("leftBtnIntro");
+
+
+    
+    // changing slides with the left and right buttons
+    rightBtnIntro.addEventListener("click", () => {
+      RL_slider("R");
+    });
+    leftBtnIntro.addEventListener("click", () => {
+      RL_slider("L");
+    });
+
+  })
+  .catch((error) => {
+    console.error(error);
+    alert("مشکلی در بار گذاری، لطفا بعدا دوباره تلاش کنید")
+  })
+
 
 // auto Slider change.
 setInterval(() => {
-  if (positionSlides == 0) {
-    sliders.forEach((value) => {
-      value.style.left = "100%"
-    })
-    introBtnClick(1)
-  }
-  else if (positionSlides == 1) {
-    sliders.forEach((value) => {
-      value.style.left = "200%"
-    })
-    introBtnClick(2)
-  }
-  else {
-    sliders.forEach((value) => {
-      value.style.left = "0%"
-    })
-    introBtnClick(0)
+  if (positionSlides == sizeDataIntro) {
+    introBtnClick(0);
+  } else {
+    introBtnClick(positionSlides + 1);
   }
 }, 10000);
-
-
 
