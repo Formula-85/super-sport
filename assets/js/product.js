85a6237303d29306892c276a6a2b22e24aafe583
Formula <ali88elsa96@gmail.com>
2025-09-06 13:10:41 +0330

    Dynamic implementation

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,7 +1,13 @@
 // =========================
 // IMPORTS
 // =========================
-import { slowingLink, offLoading, waitForImagesToLoad, englishToPrsian, discountPrice } from "./main.js";
+import {
+  slowingLink,
+  offLoading,
+  waitForImagesToLoad,
+  englishToPrsian,
+  discountPrice,
+} from "./main.js";
 
 // =========================
 // 1. VARIABLES
@@ -40,34 +46,57 @@ function createBreadcrumb(value) {
 function createProduct(value) {
   productImg.innerHTML = `<img src="${value.img}" alt="${value.imgAlt}" draggable="false">`;
 
-// if (value.discount !== "0") {
-//       informationProduct.innerHTML = `
-//         <h1>${value.title}</h1>
-//         <p class="description">${value.description}</p>
-//         <div class="discount-box">
-//             <div class="price-discount">${englishToPrsian(value.price)}</div>
-//             <div class="discount">${englishToPrsian(value.discount)}</div>
-//         </div>
-//         <div class="bottom">
-//             <div class="price">${discountPrice(value.price,value.discount)}</div>
-//             <div class="btn-buy">افزودن به سبد خرید</div>
-//         </div>
-//     `;
-// }
-// else{
-//       informationProduct.innerHTML = `
-//         <h1>${value.title}</h1>
-//         <p class="description">${value.description}</p>
-//         <div class="discount-box offDiscount">
-//             <div class="price-discount"></div>
-//             <div class="discount"></div>
-//         </div>
-//         <div class="bottom">
-//             <div class="price">${discountPrice(value.price,value.discount)}</div>
-//             <div class="btn-buy">افزودن به سبد خرید</div>
-//         </div>
-//     `;
-// }
+  if (value.discount !== "0") {
+    informationProduct.innerHTML = `
+        <h1>${value.title}</h1>
+        <p class="description">${value.description}</p>
+        <div class="features">
+          <span class="title">ویژگی های محصول :</span>
+          <ul>
+          ${value.features
+            .map((result) => {
+              return `<li>${result}</li>`;
+            })
+            .join("")}
+          </ul>
+        </div>
+        <div class="discount-box d-flex">
+          <div class="price-discount">${englishToPrsian(value.price)}</div>
+          <div class="discount"><span>${englishToPrsian(
+            value.discount
+          )}%</span></div>
+        </div>
+        <div class="bottom d-flex justify-content-between">
+          <div class="price">${discountPrice(value.price, value.discount)}</div>
+          <button>افزودن به سبد خرید</button>
+        </div>
+    `;
+  } else {
+    informationProduct.innerHTML = `
+        <h1>${value.title}</h1>
+        <p class="description">${value.description}</p>
+        <div class="features">
+          <span class="title">ویژگی های محصول :</span>
+          <ul>
+          ${value.features
+            .map((result) => {
+              return `<li>${result}</li>`;
+            })
+            .join("")}
+          </ul>
+        </div>
+        <div class="discount-box d-flex offDiscount">
+          <div class="price-discount">${englishToPrsian(value.price)}</div>
+          <div class="discount"><span>${englishToPrsian(
+            value.discount
+          )}%</span></div>
+        </div>
+        <div class="bottom d-flex justify-content-between">
+          <div class="price">${discountPrice(value.price, value.discount)}</div>
+          <button>افزودن به سبد خرید</button>
+        </div>
+    `;
+  }
 }
 
 // =========================
